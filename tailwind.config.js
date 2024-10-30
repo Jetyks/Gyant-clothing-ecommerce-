731b0d846aea7b662f96739a5adcb17d6ef4a3d6
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-10-29 18:19:39 -0600

    Agregacion de safelist en tailwind.config para correcion de errores en estilos dinamicos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -13,6 +15,18 @@ module.exports = {
       },
     },
   },
+  safelist: [
+    'text-yellow-900', 'text-blue-500', 'hover:bg-yellow-900', 'border-red-500',
+    {
+      pattern: /text-(yellow|blue|red|green|gray)-(100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /border-(yellow|blue|red|green|gray)-(100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /hover:bg-(yellow|blue|red|green|gray)-(100|200|300|400|500|600|700|800|900)/,
+    },
+  ],
   plugins: [],
-}
+};
 
