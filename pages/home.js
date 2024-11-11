fd7acea1363f3557c1fdea20097aeddc41a62f7f
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-11-10 18:56:41 -0600

    Funcionalidad en botones de vista Home para cambiar entre productos de hombre y mujer 🚀🟢🔥

--- pages/home.js
+++ pages/home.js
@@ -1,24 +1,43 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard"
 import { useProducts } from "../context/ProductsContext"
 
 
 const Home = () => {
-  const { loading, productsForHim } = useProducts();
-  /* console.log("Soy el console home", productsForHim[0].variants[0])  */
+  const { loading, productsForHim, productsForHer } = useProducts();
+  const [genderProducts, setGenderProducts] = useState([]);
+  useEffect(() => {
+    if (!loading && productsForHim) {
+      setGenderProducts(productsForHim);
+    }
+  }, [loading, productsForHim]);
+
+  const ChangeProducts = (gender) =>{
+    if(gender === 'for-him'){
+      console.log('productos para hombre')
+      setGenderProducts(productsForHim);
+    }
+    else if(gender === 'for-her'){
+      console.log('productos para mujer')
+      setGenderProducts(productsForHer);
+    }
+  }
   if (loading) {
     return <div>Cargando productos...</div>;  // Muestra un mensaje mientras carga
   }
 
-  else if (loading === false){
+  else if (loading === false && genderProducts){
     return (
       <>
       <section>
-        <div className='flex justify-center items-center gap-7 h-24 bg-gray-100 border border-black '>
-          <button className='h-auto text-xl border-b border-black text-yellow-950'>FOR HIM</button>
-          <button className='h-auto text-xl border-b border-black text-yellow-950'>FOR HER</button>
+        <div className='flex justify-center items-center gap-7 h-24 bg-gray-50 '>
+          <button className='h-auto text-xl border-b border-black text-yellow-950'
+           onClick={() => ChangeProducts('for-him')}>FOR HIM</button>
+          <button className='h-auto text-xl border-b border-black text-yellow-950' 
+           onClick={() => ChangeProducts('for-her')}>FOR HER</button>
         </div>
-          <div className="grid grid-cols-4 gap-5 px-8">
-          {productsForHim.map((product) => {
+          <div className="grid grid-cols-4 gap-5 px-8 bg-gray-50">
+          {genderProducts.map((product) => {
               const productNumber = product.product_id + 500;
               const productPrice = product.variants?.[0]?.price || "Precio no disponible";  // Validación para el precio  // Realizar la suma aquí
              /*  console.log("SOY un console en .map",product.variants) */
