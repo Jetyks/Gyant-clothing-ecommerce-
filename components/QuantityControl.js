5124a4e1bb982f5ab52d8fcd3043688bf049efe5
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-11-19 23:17:28 -0600

    Botones de componente QuantityControl para restar y agregar productos al carrito funcionales usando funciones del contexto 🟢🔥🚀

--- components/QuantityControl.js
+++ components/QuantityControl.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useCart } from '../context/CartContext'
 
-const QuantityControl = ({quantity}) => {
+const QuantityControl = ({quantity, productId, product}) => {
+    const {removeOneFromCart, addToCart} = useCart()
+    const handleRemoveFromCart = () => {
+        removeOneFromCart(productId)
+    }
+    const handleAddToCart = () => {
+        addToCart({ product_id: productId, quantity: 1 });  // Pasamos el productId y la cantidad
+    }
   return (
     <div className='flex flex-row h-full w-full border-2 border-[rgba(113,63,18,0.2)] font-medium rounded-md z-20'>
-        <div className='flex justify-center items-center h-full w-1/3 text-center text-2xl text-[rgba(66,32,6,0.8)]'>-</div>
+        <button onClick={handleRemoveFromCart} className='flex justify-center items-center h-full w-1/3 text-center text-2xl text-[rgba(66,32,6,0.8)]'>-</button>
         <div className='flex justify-center items-center h-full w-1/3 text-center text-[rgba(66,32,6,1)]'>{quantity}</div>
-        <div className='flex justify-center items-center h-full w-1/3 text-center text-2xl text-[rgba(66,32,6,0.8)]'>+</div>
+        <button onClick={handleAddToCart} className='flex justify-center items-center h-full w-1/3 text-center text-2xl text-[rgba(66,32,6,0.8)]'>+</button>
     </div>
   )
 }
