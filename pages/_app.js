e5878190f6ba6ef561409cb1ea177fb870331692
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-11-19 19:24:51 -0600

    Renderizado condicional para componente AnnouncemetBar para que no se muestre en vista /cart 🥷✖️

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ProductsProvider } from '../context/ProductsContext';
+import { useRouter } from 'next/router';
 import NavBar from "../components/NavBar"
 import '../styles/globals.css';
 import AnnouncementBar from '../components/AnnouncementBar';
@@ -6,12 +7,13 @@ import { CartProvider } from '../context/CartContext';
 
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return  (
     
       <ProductsProvider>
         <CartProvider>
           <NavBar/>
-          <AnnouncementBar/>
+          {router.pathname !== '/cart' && <AnnouncementBar />}
           <Component {...pageProps} />
         </CartProvider>
       </ProductsProvider>
