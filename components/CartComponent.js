4f50612913827b37a4b403a2099ff2069570d8dd
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-11-19 23:13:47 -0600

    Correcion de error de desajuste de hidratacion en CartComponent al recargar la pagina 🫡🔨🔥

--- components/CartComponent.js
+++ components/CartComponent.js
@@ -5,8 +5,19 @@ import CartListCard from './CartListCard';
 
 const CartComponent = () => {
   const { cart } = useCart();
-  
+  const [isMounted, setIsMounted] = useState(false); // Para evitar el desajuste de HTML
 
+  // Este effect se ejecuta una vez que el componente se ha montado en el cliente
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+   // Evitar renderizar nada antes de que el componente se haya montado
+   if (!isMounted) {
+    return null;
+  }
+  
+   // Agrupar los productos por ID y tamaño
   const groupedCartItems = cart.reduce((acc, product) => {
     const existingProduct = acc.find(item => item.product_id === product.product_id && item.size === product.size);
     
@@ -28,13 +39,14 @@ const CartComponent = () => {
         <CartListCard>
           {groupedCartItems.map((product) => {
             const productNumber = product.product_id + 500;
-            console.log(groupedCartItems)
+           /*  console.log(groupedCartItems) */
             return (
               <CartProductCard
                 key={product.product_id}
                 productId={product.product_id}
                 productNumber={productNumber}
                 productQuantity={product.quantity}
+                /* completeProduct={product} */
                 productImage={product.main_image_url}
                 productTitle={product.product_name}
                 productPrice={product.variants[0].price}
