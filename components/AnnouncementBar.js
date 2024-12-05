97480d967541b63928da3ae31882c177c286db6f
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-12-04 22:27:53 -0600

    Cambie texto de precio de la barra de AnnouncementBar

--- components/AnnouncementBar.js
+++ components/AnnouncementBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AnnouncementBar = () => {
   return (
     <div className='flex justify-center items-center h-12 w-full bg-gray-100'>
-        <p className='text-sm font-medium text-yellow-950 opacity-90'>FREE SHIPPING FOR ALL MX ORDERS OVER $60</p>
+        <p className='text-sm font-medium text-yellow-950 opacity-90'>FREE SHIPPING FOR ALL MX ORDERS OVER $80</p>
     </div>
   )
 }
